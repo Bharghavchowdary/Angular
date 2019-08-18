27df30f43d902d82da8640f98a8575c3701fb79d
Bharghav <chaudhary.bharghav4@gmail.com>
2019-08-18 20:43:07 +0530

    #imp Structural Directive - *ngIf

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -5,8 +5,20 @@ import { Component, OnInit } from '@angular/core';
   template: `<h3>
   Welcome {{name}}
   </h3>
-  <input [(ngModel)]="name"  type="text" >
- {{name}}
+  <input type="text" [(ngModel)]="name" >
+  <h2 *ngIf="name; else elseBlock ">Structural Directive</h2>  
+  <ng-template #elseBlock>
+  <h2>{{name}} Hidden</h2>
+  </ng-template>
+ 
+  <div *ngIf="name; then thenblock; else elseblock"></div>
+  <ng-template #thenblock>
+  <h2>Name Not Hidden</h2>
+  </ng-template>
+
+  <ng-template #elseblock>
+  <h2>Name Hidden</h2>
+  </ng-template>
   `,
   styles: []
 })
