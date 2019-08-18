665ae198ac664599d3188f24baa24efee28ce0c9
Bharghav <chaudhary.bharghav4@gmail.com>
2019-08-18 12:35:50 +0530

    #imp Class Binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,25 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: '[app-test]',
-  template: `<h1>
-  HAI  
-  </h1>
-  <input (change)="change()" [id]="myId" type="text" value="Bharghav">
-  <input [disabled]="isDiabled" id="{{myId}}" type="text" value="Bharghav">`,
-  styles: [`h1{
-    color:red
+  selector: 'app-test',
+  template: `<h3>
+  Welcome {{name}}
+  </h3>
+  <h4 class="text-sucess">{{name}}</h4>
+  <h4 [class]="sucessClass">{{name}}</h4>
+  <h4 [class]="sucessClass" class="text-special">{{name+" Not Applyed Styling"}}</h4>
+  <h4 [class.text-error]="hasError">{{name}}</h4>
+  <h4 [ngClass]="messageClass1">{{name}}</h4>
+  <h4 [ngClass]="messageClass2">{{name}}</h4>
+  `,
+  styles: [`.text-sucess{
+    color:green;
+  }
+  .text-error{
+    color:red;
+  }
+  .text-special{
+    font-style:italic;
   }`]
 })
 export class TestComponent implements OnInit {
-public myId="testId";
-public isDiabled=true;
+  public name="Bharghav";
+public sucessClass="text-sucess";
+public hasError=true;
+public isSpecial=true;
+public messageClass1={
+  "text-sucess":this.hasError,
+  "text-error":!this.hasError,
+  "text-special":this.isSpecial
+}
+public messageClass2={
+  "text-sucess":!this.hasError,
+  "text-error":this.hasError,
+  "text-special":this.isSpecial
+}
   constructor() { }
 
   ngOnInit() {
   }
-  change()
-  {
-    return this.isDiabled=!this.isDiabled;
-  }
+ 
 }
