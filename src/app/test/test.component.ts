af33442d9433b39ba1cfedd635b927e49694ee4e
Bharghav <chaudhary.bharghav4@gmail.com>
2019-08-18 21:07:48 +0530

    #imp *ngFor directive

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -7,17 +7,32 @@ import { Component, OnInit } from '@angular/core';
   </h3>
   <input type="text" [(ngModel)]="color" >
  
-  <div [ngSwitch]="color">
-<div *ngSwitchCase="'red'">You picked Red</div>
-<div *ngSwitchCase="'blue'">You picked Blue</div>
-<div *ngSwitchCase="'green'">You picked Green</div>
-<div *ngSwitchDefault>Pick Again</div>
+  <div *ngFor="let color of colors; index as i">
+<h2>{{i}} {{color}}</h2>
   </div>
+
+  <div *ngFor="let color of colors; first as f">
+  <h2>{{f}} {{color}}</h2>
+    </div>
+
+    <div *ngFor="let color of colors; last as l">
+  <h2>{{l}} {{color}}</h2>
+    </div>
+
+    <div *ngFor="let color of colors; odd as o">
+    <h2>{{o}} {{color}}</h2>
+      </div>
+
+      <div *ngFor="let color of colors; even as e">
+      <h2>{{e}} {{color}}</h2>
+        </div>
   `,
   styles: []
 })
 export class TestComponent implements OnInit {
  
+
+  public colors=["Red","Green","Blue","Orange"];
   constructor() { }
 
   ngOnInit() {
