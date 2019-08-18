89e70eef14583003f98f25ba16163f34ba9a6ff5
Bharghav <chaudhary.bharghav4@gmail.com>
2019-08-18 20:12:57 +0530

    #imp Twoway Binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -5,22 +5,18 @@ import { Component, OnInit } from '@angular/core';
   template: `<h3>
   Welcome {{name}}
   </h3>
-  <input #myInput type="text" >
- <button (click)=gettext(myInput.value)>Click</button>
- {{greeting}}
+  <input [(ngModel)]="name"  type="text" >
+ {{name}}
   `,
   styles: []
 })
 export class TestComponent implements OnInit {
-  public name="Bharghav";
-public greeting;
+ 
   constructor() { }
 
   ngOnInit() {
   }
-  gettext(value){
-return this.greeting=value;
-  }
+ 
  
  
 }
