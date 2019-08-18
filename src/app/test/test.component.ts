e725ce4c36eb6020f425a21532dbe974808adcec
Bharghav <chaudhary.bharghav4@gmail.com>
2019-08-18 12:48:36 +0530

    #imp Style Binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -5,37 +5,19 @@ import { Component, OnInit } from '@angular/core';
   template: `<h3>
   Welcome {{name}}
   </h3>
-  <h4 class="text-sucess">{{name}}</h4>
-  <h4 [class]="sucessClass">{{name}}</h4>
-  <h4 [class]="sucessClass" class="text-special">{{name+" Not Applyed Styling"}}</h4>
-  <h4 [class.text-error]="hasError">{{name}}</h4>
-  <h4 [ngClass]="messageClass1">{{name}}</h4>
-  <h4 [ngClass]="messageClass2">{{name}}</h4>
+  <h4 [style.color]="hasError ? 'red' : 'orange'">Style Binding 1</h4> 
+  <h4 [style.color]="highlightColor">Style Binding 2</h4> 
+  <h4 [ngStyle]="titleStyle">Style Binding 3</h4> 
   `,
-  styles: [`.text-sucess{
-    color:green;
-  }
-  .text-error{
-    color:red;
-  }
-  .text-special{
-    font-style:italic;
-  }`]
+  styles: []
 })
 export class TestComponent implements OnInit {
   public name="Bharghav";
-public sucessClass="text-sucess";
-public hasError=true;
-public isSpecial=true;
-public messageClass1={
-  "text-sucess":this.hasError,
-  "text-error":!this.hasError,
-  "text-special":this.isSpecial
-}
-public messageClass2={
-  "text-sucess":!this.hasError,
-  "text-error":this.hasError,
-  "text-special":this.isSpecial
+public hasError=false;
+public highlightColor="green";
+public titleStyle={
+  color:"blue",
+  fontStyle:"italic"
 }
   constructor() { }
 
