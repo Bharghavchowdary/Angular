bd175bf13ead57f0f84890ba6d3f7c3a1927d77b
Bharghav <chaudhary.bharghav4@gmail.com>
2019-08-18 12:04:49 +0530

    #imp Property Binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -5,16 +5,21 @@ import { Component, OnInit } from '@angular/core';
   template: `<h1>
   HAI  
   </h1>
-  <app-binding></app-binding>`,
+  <input (change)="change()" [id]="myId" type="text" value="Bharghav">
+  <input [disabled]="isDiabled" id="{{myId}}" type="text" value="Bharghav">`,
   styles: [`h1{
     color:red
   }`]
 })
 export class TestComponent implements OnInit {
-
+public myId="testId";
+public isDiabled=true;
   constructor() { }
 
   ngOnInit() {
   }
-
+  change()
+  {
+    return this.isDiabled=!this.isDiabled;
+  }
 }
