e83ab6a959f1e110c9396078d7bae8cafce74316
Bharghav <chaudhary.bharghav4@gmail.com>
2019-08-19 20:38:52 +0530

    #imp Pipes

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,28 +1,45 @@
-import { Component, OnInit, Input,EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  template: `<h3>
-  Welcome {{name}}
-  </h3>  
-  <button (click)="fireEvent($event)">Send Event</button>
+  template: `<h3>  Welcome {{name}}  </h3>  
+  <h3>   {{name | lowercase}}  </h3>
+  <h3>   {{name | uppercase}}  </h3> 
+  <h3>   {{message | titlecase}}  </h3>
+  <h3>   {{name | slice:3}}  </h3>
+  <h3>   {{name | slice:3:5}}  </h3> 
+  <h3>   {{person | json}}  </h3>
+
+  <h3>   {{5.1234 | number:'1.2-3'}}  </h3>
+  <h3>   {{5.1234 | number:'3.4-5'}}  </h3>
+  <h3>   {{5.1234 | number:'3.5-6'}}  </h3>
+
+  <h3>   {{0.25 | percent}}  </h3>
+  <h3>   {{0.25 | currency}}  </h3>
+  <h3>   {{0.25 | currency:'INR':'code'}}  </h3>
+
+  <h3>   {{date | date}}  </h3>
+  <h3>   {{date | date:'short'}}  </h3>
+  <h3>   {{date | date:'shortDate'}}  </h3>
+  <h3>   {{date | date:'shortTime'}}  </h3>
+
   `,
   styles: []
 })
 export class TestComponent implements OnInit {
  
-//From parent to child
-  //@Input() public parentData;
-  @Input('parentData') public name;
-//From Child to Parent
- @Output() public childEvent=new EventEmitter();
+public name ="Bharghav";
+public message ="Welcome to angular";
+public person ={
+  "firstName":"Siva",
+  "LastName":"Sai"
+};
+public date=new Date();
+
   constructor() { }
 
   ngOnInit() {
   }
  
-  fireEvent(event){
-this.childEvent.emit('HEY From Child '+event.type);
-  }
  
 }
